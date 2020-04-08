96fe168d3c5fd33103fb77a9624a5c1b8f042624
amir1004 <amir1004@gmail.com>
2020-04-08 12:47:02 +0300

    upload

--- resources/js/components/ProgrammeTags.vue
+++ resources/js/components/ProgrammeTags.vue
@@ -82,10 +82,9 @@
             <button class="btn btn-secondary float-left m-2" @click="open">Reset all</button>
 
             <!-- <div style="color:blue">{{findListNTag}}</div> -->
-            grouped tags : {{groupedTags}} | {{groupedTagsListName}}
-            <span
-              style="color:green"
-            >{{tagChildren}}</span>
+            <!-- grouped tags : {{groupedTags}} | {{groupedTagsListName}} -->
+            dialogBoxes : {{dialogBoxes}}
+            <span style="color:green">{{tagChildren}}</span>
           </div>
 
           <el-table @cell-mouse-enter="onRowClick" highlight-current-row :data="mainListFilter">
@@ -556,7 +555,8 @@ export default {
         tagsString =>
           tagsString.includes(name) ||
           tagsString.includes(origin_name) ||
-          _.intersection(this.dialogBoxes, tagsString.split(","))
+          (origin_name &&
+            _.intersection(this.dialogBoxes, tagsString.split(",")).length)
       );
       console.log(p_array[0]);
       console.log(this.bigJsonProgramme);
